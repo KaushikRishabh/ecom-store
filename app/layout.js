e36941b1d418f4206743079195c0f05c3a199bca
KaushikRishabh <reachmeatrish@gmail.com>
2023-08-19 22:38:50 +0530

    2

--- app/layout.js
+++ app/layout.js
@@ -1,19 +1,29 @@
-import "./globals.css";
-import { ToastContainer } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
+import "./globals.css"
+import UserProvider from './context/user'
+import CartProvider from './context/cart'
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 export const metadata = {
-  title: "ebay clone",
-  description: "ebay clone",
-};
-
+  title: 'eBay Clone',
+  description: 'eBay Clone',
+}
+ 
 export default function RootLayout({ children }) {
-  return (
+
+ return (
     <html lang="en">
       <body>
-        <ToastContainer />
-        {children}
+        <div>
+          <ToastContainer />
+
+          <UserProvider>
+            <CartProvider>
+              {children}
+            </CartProvider>
+          </UserProvider>
+        </div>
       </body>
     </html>
-  );
+  )
 }
